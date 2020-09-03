13ebc9155f722167c6929c010862df72cb6ee8bd
Jovany Candelario <jovany.candel@gmail.com>
2020-09-03 10:45:54 -0700

    Everything up to O'Tooley

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,58 @@
-Python file for PyPoll
+import os 
+import csv
+
+election_csv=os.path.join(".","Resources","election_data.csv")
+
+with open(election_csv,'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    
+    #Starting at the second row 
+    next(csvreader)
+
+    #Setting values to variables 
+    total_votes = 0
+    khan_votes = "Khan"
+    correy_votes = "Correy"
+    li_votes = "Li"
+    otooley_votes = "O'Tooley"
+    counted_khan = 0
+    counted_correy = 0
+    counted_li = 0
+    counted_otooley = 0
+    
+    for row in csvreader:
+        total_votes += 1
+
+        votes = str(row[2])
+
+        if votes == khan_votes:
+            counted_khan +=1
+        
+        khan_percentage = counted_khan/total_votes
+
+        if votes == correy_votes:
+            counted_correy +=1
+        
+        correy_percentage = counted_correy/total_votes
+
+        if votes == li_votes:
+            counted_li +=1
+
+        li_percentage = counted_li/total_votes
+
+        if votes == otooley_votes:
+            counted_otooley +=1
+        print(counted_otooley)
+        otooley_votes = counted_otooley/total_votes
+
+
+#print(f'Election Results')
+#print(f'--------------------------')
+#print(f'Total Votes: {total_votes}')
+#print(f'--------------------------')
+#print(f'Khan: {khan_percentage} ({counted_khan})')
+#print(f'Correy: {correy_percentage} ({counted_correy})')
+#print(f'Li: {li_percentage} ({counted_li})')
+#print(f"OTooley: {otooley_votes} ({counted_otooley})")
+#print(f'---------------------------')
+
